946494fb0e054dd81da15830bc5f00686cc20d23
yosh-ar <josuearguetaorozco20@gmail.com>
2019-04-28 12:42:52 -0600

    Facturacion-de-ventas-Terminado

--- vistas/js/ventas.js
+++ vistas/js/ventas.js
@@ -753,3 +753,111 @@ $(".tablas").on("click", ".btnEliminarVenta", function(){
 })
 
 
+/*=============================================
+IMPRIMIR FACTURA
+=============================================*/
+
+$(".tablas").on("click", ".btnImprimirFactura", function(){
+
+	var codigoVenta = $(this).attr("codigoVenta");
+
+	window.open("extensiones/tcpdf/pdf/factura.php?codigo="+codigoVenta, "_blank");
+
+})
+
+// /*=============================================
+// RANGO DE FECHAS
+// =============================================*/
+
+// $('#daterange-btn').daterangepicker(
+//   {
+//     ranges   : {
+//       'Hoy'       : [moment(), moment()],
+//       'Ayer'   : [moment().subtract(1, 'days'), moment().subtract(1, 'days')],
+//       'Últimos 7 días' : [moment().subtract(6, 'days'), moment()],
+//       'Últimos 30 días': [moment().subtract(29, 'days'), moment()],
+//       'Este mes'  : [moment().startOf('month'), moment().endOf('month')],
+//       'Último mes'  : [moment().subtract(1, 'month').startOf('month'), moment().subtract(1, 'month').endOf('month')]
+//     },
+//     startDate: moment(),
+//     endDate  : moment()
+//   },
+//   function (start, end) {
+//     $('#daterange-btn span').html(start.format('MMMM D, YYYY') + ' - ' + end.format('MMMM D, YYYY'));
+
+//     var fechaInicial = start.format('YYYY-MM-DD');
+
+//     var fechaFinal = end.format('YYYY-MM-DD');
+
+//     var capturarRango = $("#daterange-btn span").html();
+   
+//    	localStorage.setItem("capturarRango", capturarRango);
+
+//    	window.location = "index.php?ruta=ventas&fechaInicial="+fechaInicial+"&fechaFinal="+fechaFinal;
+
+//   }
+
+// )
+
+// /*=============================================
+// CANCELAR RANGO DE FECHAS
+// =============================================*/
+
+// $(".daterangepicker.opensleft .range_inputs .cancelBtn").on("click", function(){
+
+// 	localStorage.removeItem("capturarRango");
+// 	localStorage.clear();
+// 	window.location = "ventas";
+// })
+
+// /*=============================================
+// CAPTURAR HOY
+// =============================================*/
+
+// $(".daterangepicker.opensleft .ranges li").on("click", function(){
+
+// 	var textoHoy = $(this).attr("data-range-key");
+
+// 	if(textoHoy == "Hoy"){
+
+// 		var d = new Date();
+		
+// 		var dia = d.getDate();
+// 		var mes = d.getMonth()+1;
+// 		var año = d.getFullYear();
+
+// 		if(mes < 10){
+
+// 			var fechaInicial = año+"-0"+mes+"-"+dia;
+// 			var fechaFinal = año+"-0"+mes+"-"+dia;
+
+// 		}else if(dia < 10){
+
+// 			var fechaInicial = año+"-"+mes+"-0"+dia;
+// 			var fechaFinal = año+"-"+mes+"-0"+dia;
+
+// 		}else if(mes < 10 && dia < 10){
+
+// 			var fechaInicial = año+"-0"+mes+"-0"+dia;
+// 			var fechaFinal = año+"-0"+mes+"-0"+dia;
+
+// 		}else{
+
+// 			var fechaInicial = año+"-"+mes+"-"+dia;
+// 	    	var fechaFinal = año+"-"+mes+"-"+dia;
+
+// 		}	
+
+//     	localStorage.setItem("capturarRango", "Hoy");
+
+//     	window.location = "index.php?ruta=ventas&fechaInicial="+fechaInicial+"&fechaFinal="+fechaFinal;
+
+// 	}
+
+// })
+
+
+
+
+
+
